f85f314eb79eb17abedf81608965f30850292cd1
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-10-24 16:34:45 +0900

    feature: Handler 통합테스트, mongoDB host 변경

--- movie-review-boot/src/test/java/intg/com/lucas/moviereviewboot/router/ReviewIntgTest.java
+++ movie-review-boot/src/test/java/intg/com/lucas/moviereviewboot/router/ReviewIntgTest.java
@@ -4,6 +4,7 @@ import com.lucas.moviereviewboot.domain.Review;
 import com.lucas.moviereviewboot.repository.ReviewReactiveRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -31,7 +32,7 @@ public class ReviewIntgTest {
     @BeforeEach
     void setUp() {
         var reviewsList = List.of(
-                new Review(null, 1L, "Awesome Movie", 9.0),
+                new Review("abc", 1L, "Awesome Movie", 9.0),
                 new Review(null, 1L, "Awesome Movie1", 9.0),
                 new Review(null, 2L, "Excellent Movie", 8.0));
         repository.saveAll(reviewsList)
@@ -61,5 +62,81 @@ public class ReviewIntgTest {
                });
     }
 
+    // FindAll
+    @Test
+    @DisplayName("모든 리뷰 조회")
+    void findAll() {
+        webTestClient
+                .get()
+                .uri(REVIEWS_INFO_URL)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBodyList(Review.class)
+                .value(reviews -> {
+                    assertEquals(3, reviews.size());
+                });
+    }
 
+    // FindById
+    @Test
+    void getReviewByMovieInfoId() {
+        webTestClient
+                .get()
+                .uri(uriBuilder -> {
+                    return uriBuilder.path(REVIEWS_INFO_URL)
+                            .queryParam("movieInfoId", 1)
+                            .build();
+                })
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBodyList(Review.class)
+                .value(list -> {
+                    System.out.println("list = " + list);
+                    assertEquals(2, list.size());
+                });
+    }
+
+    // Update Review
+    @Test
+    @DisplayName("리뷰 수정")
+    void updateReview() {
+        //given
+        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+        var savedReview = repository.save(review).block();
+        var reviewUpdate = new Review(null, 1L, "Not an Awesome Movie", 8.0);
+        //when
+        assert savedReview != null;
+
+        webTestClient
+                .put()
+                .uri(REVIEWS_INFO_URL+"/{id}", savedReview.getReviewId())
+                .bodyValue(reviewUpdate)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Review.class)
+                .consumeWith(reviewResponse -> {
+                    var updatedReview = reviewResponse.getResponseBody();
+                    assert updatedReview != null;
+                    System.out.println("updatedReview : " + updatedReview);
+                    assertNotNull(savedReview.getReviewId());
+                    assertEquals(8.0, updatedReview.getRating());
+                    assertEquals("Not an Awesome Movie", updatedReview.getComment());
+                });
+    }
+
+    // Delete Review
+    @Test
+    @DisplayName("리뷰 삭제")
+    void deleteReview() {
+        //given
+        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+        var savedReview = repository.save(review).block();
+        //when
+        assert savedReview != null;
+        webTestClient
+                .delete()
+                .uri(REVIEWS_INFO_URL+"/{id}", savedReview.getReviewId())
+                .exchange()
+                .expectStatus().isNoContent();
+    }
 }//class
